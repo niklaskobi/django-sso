efa311fede6345131f39a96a90e3b56350f35510
DAVIDhaker <DAVIDhaker@yandex.ru>
2022-08-22 23:15:14 +0300

    Update version number for update in PYPI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_sso',
-    version='1.1.0',
+    version='1.1.2',
     author="DAVIDhaker",
     author_email='me@davidhaker.ru',
     packages=find_packages('src'),
